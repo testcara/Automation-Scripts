a552016e882bc01695274ac5e32a6a1637175150
Cara Wang <wlin@redhat.com>
2017-06-28 15:06:59 +0800

    Fix the repo name problem

--- update_pulp_docker.sh
+++ update_pulp_docker.sh
@@ -3,8 +3,9 @@ echo "==installing the pub qa tools=="
 echo "yum install -y dockpulp rh-dockpulp"
 yum install -y dockpulp rh-dockpulp
 echo "==writing the /etc/dockerpulp.conf=="
-vim -c ":1,$ d" -c ":wq" /etc/dockerpulp.conf
-echo "[pulps]
+vim -c ":1,$ d" -c ":wq" /etc/dockpulp.conf
+echo "
+[pulps]
 test = https://pulp-docker-wlin.usersys.redhat.com
  
 [registries]
@@ -17,7 +18,7 @@ test = http://10.73.67.150
 test = no
  
 [redirect]
-test = yes
+test = no
  
 [distributors]
 test = docker_web_distributor_name_cli,cdn_distributor,cdn_distributor_unprotected
@@ -29,13 +30,14 @@ test = cdn_distributor_no_extra,cdn_distributor_unprotected_no_extra,docker_web_
 test = 10
  
 [timeout]
-test = 5000" >> /etc/dockerpulp.conf
+test = 5000
+" >> /etc/dockpulp.conf
 echo "==Outputting the dockerpulp.conf files"
 cat /etc/dockerpulp.conf
 echo "==creating some repos=="
 echo "dock-pulp -s test login -u admin -p admin"
 dock-pulp -s test login -u admin -p admin
-for  item  in   redhat-rhel7.3  redhat-rhel   redhat-rhel7  redhat-rhel7-rhel
+for  item  in   rhel7.3  rhel   rhel7  rhel7-rhel
 do
 echo "dock-pulp -s test create --library  ${item}"
 dock-pulp -s test create --library  ${item}
